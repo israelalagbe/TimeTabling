9f5ff568034f298e7460fa5e987b32bb110c224c
Israel Alagbe <israelalagbe53@gmail.com>
2019-08-10 03:45:17 +0100

    Add lecturer's model

---
+++ src/main/java/com/israelalagbe/timetable/models/Lecturer.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.israelalagbe.timetable.models;
+import cat.quickdb.annotation.Column;
+import cat.quickdb.annotation.ColumnDefinition;
+import cat.quickdb.annotation.Definition.DATATYPE;
+import cat.quickdb.annotation.Properties.TYPES;
+import cat.quickdb.annotation.Table;
+/**
+ *
+ * @author Israel Alagbe
+ */
+@Table("Lecturers")
+public class Lecturer {
+    @Column(type = TYPES.PRIMARYKEY)
+    @ColumnDefinition(autoIncrement = true, length = 11,
+            primary = true, type = DATATYPE.INT)
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String gender;
+    private String dob;
+    public int getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+    
+    
+    
+    
+}
--- src/main/java/com/israelalagbe/timetable/models/TimeTable.java
+++ src/main/java/com/israelalagbe/timetable/models/TimeTable.java
@@ -4,18 +4,21 @@
  * and open the template in the editor.
  */
 package com.israelalagbe.timetable.models;
+
 import cat.quickdb.annotation.Column;
 import cat.quickdb.annotation.ColumnDefinition;
 import cat.quickdb.annotation.Definition;
 import cat.quickdb.annotation.Definition.DATATYPE;
 import cat.quickdb.annotation.Properties.TYPES;
 import cat.quickdb.annotation.Table;
+
 /**
  *
  * @author Israel Alagbe
  */
 @Table("TimeTables")
 public class TimeTable {
+
     @Column(type = TYPES.PRIMARYKEY)
     @ColumnDefinition(autoIncrement = true, length = 11,
             primary = true, type = DATATYPE.INT)
@@ -25,7 +28,8 @@ public class TimeTable {
     private Department department;
     private String day;
     private String time;
-    
+    private Lecturer lecturer;
+
     public int getId() {
         return id;
     }
@@ -49,6 +53,7 @@ public class TimeTable {
     public Department getDepartment() {
         return department;
     }
+
     public void setDepartment(Department department) {
         this.department = department;
     }
@@ -56,6 +61,7 @@ public class TimeTable {
     public String getDay() {
         return day;
     }
+
     public void setDay(String day) {
         this.day = day;
     }
@@ -63,11 +69,17 @@ public class TimeTable {
     public String getTime() {
         return time;
     }
+
     public void setTime(String time) {
         this.time = time;
     }
-    
-    
-    
-    
+
+    public Lecturer getLecturer() {
+        return lecturer;
+    }
+
+    public void setLecturer(Lecturer lecturer) {
+        this.lecturer = lecturer;
+    }
+
 }
