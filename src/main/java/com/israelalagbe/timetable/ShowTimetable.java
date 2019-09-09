90f06500f05a86720fff2b28f6401ca986969cc0
Israel Alagbe <israelalagbe53@gmail.com>
2019-09-09 22:13:32 +0100

    Got timetable table working

--- src/main/java/com/israelalagbe/timetable/ShowTimetable.java
+++ src/main/java/com/israelalagbe/timetable/ShowTimetable.java
@@ -14,6 +14,7 @@ import com.israelalagbe.timetable.models.TimeTable;
 import com.jfoenix.controls.JFXComboBox;
 import com.jfoenix.controls.JFXTextField;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
 import javafx.collections.FXCollections;
@@ -28,7 +29,9 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.RowConstraints;
+import javafx.scene.layout.VBox;
 
 /**
  *
@@ -43,6 +46,9 @@ public class ShowTimetable extends BaseController{
     private JFXComboBox<Level>  levels;
      @FXML
     private GridPane  grid;
+      String[] days={
+            "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"
+        };
 
     @Override
     public void loaded() throws Exception {
@@ -50,32 +56,27 @@ public class ShowTimetable extends BaseController{
         levels.setItems(obs(mainApp.dataService.getModels(new Level())));
         grid.setPadding(new Insets(50,50,50,50));
         grid.setAlignment(Pos.CENTER);
-        String[] days={
-            "Moday", "Tuesday", "Wednesday", "Thursday", "Friday"
-        };
+       
        
             for (int col = 0; col <= 4; col++) {
                
                 Label label=new Label(days[col]);
                 addItem(label,0, col);
             }
-//        for (int row = 0; row < 10; row++) {
-//            for (int col = 0; col < 10; col++) {
-//               
-//                Label label=new Label("Monday");
-//                addItem(label, col, row);
-//                
-//            }
-//        }
     }
     void addItem(Label label, int row,int col){
-        RowConstraints rc=new RowConstraints(50);
-                 ColumnConstraints column1 = new ColumnConstraints(100);
+        RowConstraints rc=new RowConstraints(70);
+                 ColumnConstraints column1 = new ColumnConstraints(150);
                  column1.setHalignment(HPos.CENTER);
                  rc.setValignment(VPos.CENTER);
                 grid.getColumnConstraints().add(column1);
                 grid.getRowConstraints().add(rc);
-                grid.add(label, col, row);
+                
+                VBox pane=new VBox(label);
+                pane.setAlignment(Pos.CENTER);
+                pane.setStyle("-fx-border-color:black;");
+               
+                grid.add(pane, col, row);
     }
     @FXML
     private void filterTimetable(ActionEvent event) {
@@ -85,8 +86,44 @@ public class ShowTimetable extends BaseController{
             UIManager.showAlert(Alert.AlertType.ERROR, mainApp.getWindow(), "Error", "Info cannot be empty!" );
                 return;
         }
+        
         List <TimeTable>list=mainApp.dataService.getTimetables(department, level);
-        System.out.println("Timetable lenght : "+list.size());
+          List children=grid.getChildren();
+        grid.getChildren().clear();
+        grid.setPadding(new Insets(50,50,50,50));
+        grid.setAlignment(Pos.CENTER);
+        for (int col = 0; col <= 4; col++) {
+               
+                Label label=new Label(days[col]);
+                
+                addItem(label,0, col);
+            }
+        for (int i = 0; i < days.length; i++) {
+            displayTimetableDay(list, days[i], i);
+        }
+        grid.setGridLinesVisible(true);
+        
+//        System.out.println("Timetable lenght : "+list.size());
+    }
+    void displayTimetableDay(List <TimeTable>allTimetables, String day,int col){
+        List <TimeTable>dayList=new ArrayList<>();
+        for (TimeTable timeTable : allTimetables) {
+            if(timeTable.getDay().equals(day)){
+                dayList.add(timeTable);
+            }
+        }
+        int row = 0;
+        for (; row < dayList.size(); row++) {
+            TimeTable timeTable =dayList.get(row) ;
+            Label label=new Label(timeTable.getCourse().getName()+
+                    "\n Time: "+ timeTable.getTime() +
+                    "\n Duration: " + timeTable.getDuration() +" Hour(s)");
+            addItem(label,row+1, col);
+        }
+        for(;row<10;row++){
+            Label label=new Label("");
+             addItem(label,row+1, col);
+        }
     }
 
     @Override
